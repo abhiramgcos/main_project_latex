4f5a1a5a594772e67c99edf7d9dce375424247e9
Abhiram G Nair <munnaabhiram321@gmail.com>
2026-02-22 21:03:29 +0530

    feat: Add a second `pdflatex` compilation pass to ensure all references are resolved.

--- compile.bat
+++ compile.bat
@@ -2,5 +2,6 @@
 echo Compiling LaTeX document...
 if not exist logs mkdir logs
 pdflatex -output-directory=logs -interaction=nonstopmode main.tex
+pdflatex -output-directory=logs -interaction=nonstopmode main.tex
 move /Y logs\main.pdf .
 echo Done!
