1003fa26451068ff7f9459f3836a3bf8245d7f21
Abhiram G Nair <munnaabhiram321@gmail.com>
2026-02-25 23:39:27 +0530

    feat: Migrate LaTeX compilation to xelatex and introduce new font and general utility packages.

--- compile.sh
+++ compile.sh
@@ -2,17 +2,17 @@
 echo "Compiling LaTeX document..."
 mkdir -p logs/frontmatter logs/chapters
 
-echo "Pass 1: pdflatex..."
-pdflatex -output-directory=logs -interaction=nonstopmode main.tex
+echo "Pass 1: xelatex..."
+xelatex -output-directory=logs -interaction=nonstopmode main.tex
 
 echo "Pass 2: bibtex..."
 bibtex logs/main
 
-echo "Pass 3: pdflatex..."
-pdflatex -output-directory=logs -interaction=nonstopmode main.tex
+echo "Pass 3: xelatex..."
+xelatex -output-directory=logs -interaction=nonstopmode main.tex
 
-echo "Pass 4: pdflatex..."
-pdflatex -output-directory=logs -interaction=nonstopmode main.tex
+echo "Pass 4: xelatex..."
+xelatex -output-directory=logs -interaction=nonstopmode main.tex
 
 mv -f logs/main.pdf .
 echo "Done!"
